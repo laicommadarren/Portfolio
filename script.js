69d33baa35c7796c70314e0096127f5a1ad05eec
laicommadarren <132854811+laicommadarren@users.noreply.github.com>
2023-06-28 22:25:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var navButton = document.querySelector(".navButton");
+var navMobile = document.querySelector(".navMobile");
+
+function openNav() {
+  navButton.style.visibility = "hidden";
+  navMobile.style.visibility = "visible";
+}
+
+function closeNav() {
+  navMobile.style.visibility = "hidden";
+  navButton.style.visibility = "visible";
+}
+
+function showFunFact() {
+  document.querySelector(".funFact").style.visibility = "visible";
+}
+
+function email() {
+  window.open("mailto:nerdzrull4@gmail.com", "_blank")
+}
+
+function linkedin() {
+  window.open("https://www.linkedin.com/in/laicommadarren/", "_blank");
+}
+
+function github() {
+  window.open("https://www.github.com/laicommadarren", "_blank");
+}
+
+function instagram() {
+  window.open("https://www.instagram.com/darren_unltd")
+}
+
+function youtube() {
+  window.open("https://www.youtube.com/@NomzWithDarren/videos", "_blank")
+}
+
+function yelp() {
+  window.open("https://www.yelp.com/user_details?userid=48gUWtqMot6FzU7k3eclKw", "_blank")
+}
